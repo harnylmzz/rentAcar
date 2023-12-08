0a4c230982e7b2a896ff51c07859558fdcdbd68d
Harun Yılmaz <haruny232@aof.anadolu.edu.tr>
2023-12-08 18:13:09 +0300

    global handle exception

--- src/main/java/com/tobeto/rentAcar/core/utilities/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/tobeto/rentAcar/core/utilities/exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,38 @@
 package com.tobeto.rentAcar.core.utilities.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({MethodArgumentNotValidException.class})
-    public String handleValidationException() {
-        return "Validation error";
+    public Map<String, String> handleValidationException(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return errors;
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleRuntimeException(RuntimeException exception) {
+        return exception.getMessage();
+    }
+
+    @ExceptionHandler({Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleUnknownException() {
+        return "Unknown error occurred";
     }
 }
